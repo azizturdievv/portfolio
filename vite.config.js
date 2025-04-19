67cd7ead8371f39e26f074d4995ef66f4c11f120
Aziz Turdiev <86876217+azizturdievv@users.noreply.github.com>
2025-04-19 11:47:45 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-dom/client'],
+      external: ['react-dom/client'], // "react-dom/client" modulini tashqi bog'liq qilib belgilash
     },
   },
 })
